c79386f4a1621c8dd8e96eb25d61f2bc1f72b41f
Rendleks <0rendl050@gmail.com>
2022-12-12 14:54:17 +0300

    add help

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def help():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
