b0149a95ec8e5ee39121057f0e23272059a74730
Rendleks <0rendl050@gmail.com>
2022-12-13 15:10:34 +0300

    add generate_diff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
-import gendiff
 import json
+import pprint
 
-print(gendiff.parser_args)
+
+def generate_diff(first_file, second_file):
+    first_file = json.load(open(first_file))
+    second_file = json.load(open(second_file))
+    result = {}
+    common_keys = sorted(first_file | second_file)
+    print(common_keys)
+
+    for key in common_keys:
+        if key in first_file and key in second_file:
+            if first_file[key] == second_file[key]:
+                result[f"  {key}"] = first_file[key]
+            else:
+                result[f"- {key}"] = first_file[key]
+                result[f"+ {key}"] = second_file[key]
+        elif key in first_file and key not in second_file:
+            result[f"- {key}"] = first_file[key]
+        elif key not in first_file and key in second_file:
+            result[f"+ {key}"] = second_file[key]
+        
+    return result
+    
+
+
+pprint.pprint(generate_diff("C:\\Users\\internetshop17\\pars\\python-project-50\\gendiff\\scripts\\file1.json", "C:\\Users\\internetshop17\\pars\\python-project-50\\gendiff\\scripts\\file2.json"))
 
 
 
