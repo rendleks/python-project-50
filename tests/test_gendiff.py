eaa5c873a327cb8ea221f5115a1a176442375171
rendleks <0rendl050@gmail.com>
2023-02-07 23:29:33 +0300

    add test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,23 +1,33 @@
-from gendiff import generate_diff
-import json
+import pytest
+from gendiff.generate_diff import stringify
 import os
 
 
-def test_simple():
-    file1 = json.load(open(os.path.join('tests', 'fixtures', 'file1.json')))
-    file2 = json.load(open(os.path.join('tests', 'fixtures', 'file2.json')))
+def get_fixture_path(file_name):
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+  return os.path.join(current_dir, 'fixtures', file_name)
 
-    expected = """
-    {
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-    }
-    """
 
-    print(generate_diff.stringify(file1, file2))
+def read(file_path):
+  with open(file_path, 'r') as f:
+      result = f.read()
+  return result
 
-test_simple()
+
+file1 = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": False,
+}
+
+
+file2 = {
+  "timeout": 20,
+  "verbose": True,
+  "host": "hexlet.io",
+}
+
+simple_data = read(get_fixture_path('simple.txt')).rstrip().split('\n\n\n')
+
+print(simple_data)
